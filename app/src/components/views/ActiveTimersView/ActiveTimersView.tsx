d8dbd548798704ec8c8f54c3e21b66e4c82c3d9b
Pontus Persson <pontus.persson@einride.tech>
2020-12-20 16:44:38 +0100

    Swipe away timers

--- app/src/components/views/ActiveTimersView/ActiveTimersView.tsx
+++ app/src/components/views/ActiveTimersView/ActiveTimersView.tsx
@@ -2,6 +2,8 @@ import React, { useCallback, useState } from "react";
 import { DateTime, Duration } from "luxon";
 import * as emoji from "lib/emoji";
 import { useLiveTime } from "components/hooks/useLiveTime";
+import SwipeList from "components/ui/SwipeList";
+import SwipeItem from "components/ui/SwipeList/components/SwipeItem";
 import TimerComp from "./components/Timer";
 import { Timer } from "types/Timer";
 import "./ActiveTimersView.css";
@@ -30,23 +32,32 @@ const ActiveTimersView: React.FC<ActiveTimersViewProps> = ({}) => {
   return (
     <div className="ActiveTimersView">
       <div className="timers">
-        {timers.map((timer, index) => {
-          const handleChange = (timer: Timer) => {
-            setTimers((timers) =>
-              timers.map((t, i) => {
-                if (index === i) {
-                  return timer;
-                }
-                return t;
-              })
+        <SwipeList>
+          {timers.map((timer, index) => {
+            const handleChange = (timer: Timer) => {
+              setTimers((timers) =>
+                timers.map((t, i) => {
+                  if (index === i) {
+                    return timer;
+                  }
+                  return t;
+                })
+              );
+            };
+
+            const handleRemove = () => {
+              setTimers((timers) => timers.filter((t) => t.id !== timer.id));
+            };
+
+            return (
+              <SwipeItem key={timer.id} onSwipeAway={handleRemove}>
+                <div className="timer">
+                  <TimerComp now={now} timer={timer} onChange={handleChange} />
+                </div>
+              </SwipeItem>
             );
-          };
-          return (
-            <div key={timer.id} className="timer">
-              <TimerComp now={now} timer={timer} onChange={handleChange} />
-            </div>
-          );
-        })}
+          })}
+        </SwipeList>
       </div>
       <div className="add">
         <button type="button" onClick={addTimer}>
